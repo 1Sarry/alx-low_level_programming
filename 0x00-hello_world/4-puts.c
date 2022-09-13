3a51d208f4132f5caf3a98501f46ebbf979cab0f
1Sarry <saronmeles4@gmail.com>
2022-09-12 18:13:17 -0700

    Betty documentation style fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 /**
- * a C programm that prints the given out put
- * "Programming is like building a multilingual puzzle"
  * main - prints the output
  * return: 0 if exited properly
  */
