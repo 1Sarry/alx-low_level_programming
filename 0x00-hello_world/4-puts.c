af3d53eaac4c50175d6661128200c72344ad671c
1Sarry <saronmeles4@gmail.com>
2022-09-12 18:26:04 -0700

    Betty style error fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * main -prints the phrase "Programming is like building a multilingual puzzle
  * return: 0 if exited properly, otherwise non zero
  */
 int main(void)
