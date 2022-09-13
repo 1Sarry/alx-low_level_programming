3d574e7d1b2bde621102abbf52e319562ad91e11
1Sarry <saronmeles4@gmail.com>
2022-09-12 17:01:31 -0700

    fix the code according to betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-/* a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.*/
+/**
+ *  a C program that prints exactly
+ *   "Programming is like building a multilingual puzzle, 
+ *   followed by a new line.
+ *   main - prints the output
+ *   return: 0 if exited properly
+ */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+       	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
